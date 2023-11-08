40c35c13abc4bfd21b33106830b81fc652139d0d
mikk39k1 <mikk39k1@stud.kea.dk>
2023-11-08 19:56:01 +0100

    Added scrollable Display messages

--- src/components/chatroom/DisplayMessages.tsx
+++ src/components/chatroom/DisplayMessages.tsx
@@ -5,7 +5,7 @@ export const DisplayMessages = ({ chatMessages }: { chatMessages: ChatMessage[] 
     <div>
       {chatMessages.map((message, index) => (
         <div key={index}>
-        <p style={{ wordWrap: "break-word" }}>
+        <p className="my-4" style={{ wordWrap: "break-word" }}>
             {message.textMessage}
         </p>
         </div>
